a3037c50a45abb6024c277d628f95286819eab19
Luka Collier <98172286+LukaCollier@users.noreply.github.com>
2025-12-02 15:11:11 +0100

    Add files via upload

--- projet_Luka/neuroneopti.py
+++ projet_Luka/neuroneopti.py
@@ -20,10 +20,18 @@ class Layer:
         self.z=np.zeros(n_neurone)
         self.f=np.zeros(n_neurone)
         self.activ=activ
-    def forward(self,x):
-        self.x=np.array(x)
-        self.z=self.w @ x + self.biais
-        self.f=self.activ.function(self.z)
+    
+    def forward(self, x):
+        # x est de shape: (n_input,) ou (n_input, batch_size)
+        self.x = np.array(x)
+        
+        # Si x est (n_input,)
+        if self.x.ndim == 1:
+            self.x = self.x.reshape(1, -1)
+        
+        # z = W @ x + b (broadcast le biais)
+        self.z = self.w @ self.x + self.biais.reshape(-1, 1)
+        self.f = self.activ.function(self.z)
         return self.f
         
         
@@ -42,43 +50,52 @@ class Neural_Network:
         for i,nb_n in enumerate(nb_n_l):
             self.l.append(Layer(n_input,nb_n,activ[i]))
             n_input=nb_n
+            
     def forward(self,x):
-        self.a = [np.array(x)] #self.a=[x]
+        x = np.array(x)
+        if x.ndim == 1:
+            x = x.reshape(1, -1)
+        
+        self.a = [x]
         for neu in self.l:
             x=neu.forward(x)
             self.a.append(x)
         return x
+    
     def MSE(self,y_pred,y):
         return 0.5*np.sum((y_pred-y)**2)
+        
     def backward(self,y,lr):
         '''
         y : résultat attendu possiblement |y|> 1 si plusieurs neurones sur la couche la plus haute
         lr : pas de déplacement des poids et biais
         Principe d'abord calcul delta pour la dernière couche car ne peut être mis dans une boucle récursive puis itéré jusqu'à la première couche
-        Memo:
-        np.outer : réalise un produit externe de deux vecteur 1D  a,b de taille respective n et m. Renvoie une matrice (n,m) avec
-        c_ij=a_i *b_j
-        permet de faire l'équivalent du produit d'une matrice (n,1) et d'une matrice (1,m)
         '''
-        y_pred=self.a[-1]
         y = np.array(y, dtype=float)
+        if y.ndim == 1:
+            y = y.reshape(1, -1)
+        
+        y_pred=self.a[-1]
         L = self.nbl - 1
         delta=[None]*(L+1)
+        
         #couche la plus haute
         neu=self.l[-1]
         delt=(y_pred-y)*neu.activ.derivative(neu.z)
         delta[-1]=delt
+        
         for i in range(L-1,-1,-1):
             neu=self.l[i]
             next_neu=self.l[i+1]
             delt=(next_neu.w.T @ delta[i+1])*neu.activ.derivative(neu.z)
             delta[i]=delt
+            
         for (i,neu) in enumerate(self.l):
-            neu.w-=lr*np.outer(delta[i],self.a[i]) #on réalise un produit exterieur car delta[i] à pour dimension (h^(i),) et self.a[i] à pour dimension (h^(i-1),)
-            neu.biais-=lr*delta[i]
+            # Gradient: delta[i] est de shape (n_neurons, batch_size) self.a[i] est de shape (n_inputs, batch_size)
+            # Produit matriciel: (n_neurons, batch_size) @ (batch_size, n_inputs) = (n_neurons, n_inputs)
+            grad_w = delta[i] @ self.a[i].T
+            grad_b = np.sum(delta[i], axis=1) #permet d'éviter les problèmes et de perdre 1H X)
+            
+            neu.w -= lr * grad_w
+            neu.biais -= lr * grad_b
         return y_pred
-    
-
-
-
-         
